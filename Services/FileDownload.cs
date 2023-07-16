59f02c4e093ae2ef4a34d5f44d0cfa55dfedc037
Vitalii <seminenkovs@gmail.com>
2023-07-16 21:50:10 +0200

    project upload

---
+++ Services/FileDownload.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.JSInterop;
+
+
+namespace BlazorFileUpload.Services
+{
+    public interface IFIleDownload
+    {
+        Task<List<String>> GetUploadedFiles();
+        Task DownloadFile(string url);
+    }
+
+    public class FileDownload : IFIleDownload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly IJSRuntime _js;
+
+        public FileDownload(IWebHostEnvironment webHostEnvironment, IJSRuntime js)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _js = js;
+        }
+
+        public async Task<List<string>> GetUploadedFiles()
+        {
+            var base64urls = new List<string>();
+            var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+            var files = Directory.GetFiles(uploadPath); 
+
+            if (files is not null && files.Length > 0)
+            {
+                foreach (var file in files) 
+                {
+                    using (var fileInput = new FileStream(file, FileMode.Open, FileAccess.Read))
+                    { 
+                        var memorystream = new MemoryStream();
+                        await fileInput.CopyToAsync(memorystream);
+
+                        var buffer = memorystream.ToArray();
+                        var fileType = GetMimeTypeForFileExtension(file);
+                        base64urls.Add($"data:{fileType}; base64,{Convert.ToBase64String(buffer)}");
+                    }
+                }
+            }
+
+            return base64urls;
+        }
+
+        public string GetMimeTypeForFileExtension(string filePath)
+        {
+            const string DefaultContentType = "application/octet-stream";
+
+            var provider = new FileExtensionContentTypeProvider();
+
+            if (!provider.TryGetContentType(filePath, out string contentType))
+            {
+                contentType = DefaultContentType;
+            }
+
+            return contentType;
+        }
+
+        public async Task DownloadFile(string url)
+        {
+            await _js.InvokeVoidAsync("downloadFile", url);
+        }
+    }
+}
---
+++ Services/FileUpload.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorFileUpload.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadFile(IBrowserFile file);
+        Task<string> GeneratePreviewUrl(IBrowserFile file); 
+    }
+
+    public class FileUpload : IFileUpload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUpload> _logger;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
+        public async Task UploadFile(IBrowserFile file)
+        {
+            if (file is not null)
+            {
+                try
+                {
+                    var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", file.Name);
+
+                    using (var stream =  file.OpenReadStream())
+                    {
+                        var fileStream = File.Create(uploadPath);
+                        await stream.CopyToAsync(fileStream);
+                        fileStream.Close();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogError(ex.ToString());
+                }
+            }
+        }
+
+        public async Task<string> GeneratePreviewUrl(IBrowserFile file)
+        {
+            if (!file.ContentType.Contains("image"))
+            {
+                if (file.ContentType.Contains("pdf"))
+                {
+                    return "images/pdf_logo.png";
+                }
+            }
+
+            var resizeImage = await file.RequestImageFileAsync(file.ContentType, 100, 100);
+            var buffer = new byte[resizeImage.Size];
+            await resizeImage.OpenReadStream().ReadAsync(buffer);
+            return $"data:{file.ContentType}; base64{Convert.ToBase64String(buffer)}";
+        }
+    }
+}
